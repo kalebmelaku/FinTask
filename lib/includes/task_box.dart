a9696770ecbfd5f006b88784094b07037e5c2918
enusiraw <edlsiraw@gmail.com>
2024-05-09 00:28:48 +0300

    Add date selector and see more link

--- lib/includes/task_box.dart
+++ lib/includes/task_box.dart
@@ -4,59 +4,113 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 
-class TaskBox extends StatelessWidget {
+class TaskBox extends StatefulWidget {
   late Future<List<dynamic>> tasks;
+
   TaskBox({super.key, required this.tasks});
 
   @override
+  State<TaskBox> createState() => _TaskBoxState();
+}
+
+class _TaskBoxState extends State<TaskBox> {
+  @override
   Widget build(BuildContext context) {
+    DateTime date = DateTime.now();
     return Expanded(
-      child: Container(
-        width: double.infinity,
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-            color: MyColors.tertiaryColor,
-            borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(0),
-                topRight: Radius.circular(15),
-                bottomLeft: Radius.circular(0),
-                bottomRight: Radius.circular(0))),
-        child: FutureBuilder(
-          future: tasks,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Text('Error:  ${snapshot.error}'),
-              );
-            } else {
-              List<dynamic> tasks = snapshot.data as List<dynamic>;
-              return ListView(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
+        child: Container(
+            width: double.infinity,
+            //padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                color: MyColors.tertiaryColor,
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(0),
+                    topRight: Radius.circular(15),
+                    bottomLeft: Radius.circular(0),
+                    bottomRight: Radius.circular(0))),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  ...(tasks).map((e) {
-                    return CustomTile(
-                      taskName: e['name'],
-                      date: e['due_date'],
-                    );
-                  })
+                  TextButton(
+                    onPressed: () async {
+                      DateTime? newDate = await showDatePicker(
+                        context: context,
+                        initialDate: date,
+                        firstDate: DateTime(1990),
+                        lastDate: DateTime(2100),
+                      );
+                      if (newDate != null) {
+                        setState(() {
+                          date = newDate;
+                        });
+                      }
+                    },
+                    child: Text(
+                      '${date != null ? date.year : DateTime.now().year} - ${date != null ? date.month : DateTime.now().month} - ${date != null ? date.day : DateTime.now().day}',
+                      style: TextStyle(fontSize: 11.sp, color: Colors.white),
+                    ),
+                  ),
+      
+                     MaterialButton(
+                      onPressed: () {
+                        Navigator.of(context).pushNamed("/homecontroller");
+                      },
+                      color: MyColors.primaryColor,
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Text(
+                        'See More',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 11.sp,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  
                 ],
-              );
-            }
-          },
-        ),
-      ),
-    );
+              ),
+              SizedBox(height: 10.h),
+              Expanded(
+                child: FutureBuilder(
+                  future: widget.tasks,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    } else if (snapshot.hasError) {
+                      return Center(
+                        child: Text('Error:  ${snapshot.error}'),
+                      );
+                    } else {
+                      List<dynamic> tasks = snapshot.data as List<dynamic>;
+                      return ListView(
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        children: [
+                          ...(tasks).map((e) {
+                            return CustomTile(
+                              taskName: e['name'],
+                              date: e['due_date'],
+                            );
+                          })
+                        ],
+                      );
+                    }
+                  },
+                ),
+              ),
+            ])));
   }
 
   Widget CustomTile({required taskName, required date}) {
     DateTime dateTime = DateTime.parse(date);
     String formattedDate = DateFormat("y-MM-DD").format(dateTime.toLocal());
-   
+
     return Column(
       children: [
         Container(
